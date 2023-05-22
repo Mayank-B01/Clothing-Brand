ed994a3e3ac4ed3012afb6c70b201703b5e8a7e1
Mayank-B01 <np03cs4s230205@heraldcollege.edu.np>
2023-05-22 23:52:54 +0545

    website

---
+++ js/nav.js
@@ -0,0 +1,13 @@
+const header = document.querySelector("header");
+
+window.addEventListener ("scroll", function(){
+    header.classList.toggle ("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navigation = document.querySelector('.navigation');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navigation.classList.toggle('open'); 
+};
